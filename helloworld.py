22f1966a6fddfd775d5cc27fadd2a9d74782e4b8
hmcgill <147208766+hmcgilll@users.noreply.github.com>
2025-01-09 11:54:24 -0800

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,2 @@
+print("Hello World! I am in CIS141 and following the videos! Yes this is unique")
+# This is the first code for CIS141 so looking forward to learning more. also I hope you like my music choice. 
