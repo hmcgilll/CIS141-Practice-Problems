6c3834ce0f908b32908141fec84a9f7892050ab4
hmcgill <147208766+hmcgilll@users.noreply.github.com>
2025-01-17 12:36:07 -0800

    Update mod1-problems.py

--- mod1-problems.py
+++ mod1-problems.py
@@ -18,3 +18,21 @@ s2= int(s*((s-firstnumber)+(s-secondnumber)+(s-thirdnumber)))
 a=math.sqrt(s2)
 print(a)
 # 4. Create a program that computes different statistics given five numbers including the total, average, minimum, maximum, range, and standard deviation (https://en.wikipedia.org/wiki/Standard_deviation).
+import math
+numbers= []
+for i in range(5):
+    num = int(input("Enter a number"))
+    numbers.append(num)
+total= sum(numbers)
+average= total/len(numbers)
+minimum= min(numbers)
+maximum=max(numbers)
+range_value= maximum - minimum
+deviation= sum((x - average) ** 2 for x in numbers) / len(numbers)
+standard= math.sqrt(deviation)
+print(total)
+print(average)
+print(minimum)
+print(maximum)
+print(range_value)
+print(standard)
